9fb2fa8a01ee36e65ee3831d7e483b99f2c448d5
Francisco Esparza <francisco.esparza@murciaeduca.es>
2023-01-16 16:23:49 +0100

    Aplicación carga configuración, conecta con la base de datos y muestra ventana vacia

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.profesor.pelis {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.driver.sync.client;
+
     requires org.kordamp.ikonli.javafx;
 
     opens com.profesor.pelis to javafx.fxml;
